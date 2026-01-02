5bfb575934e2f922d04a6ae15f356401e1af79b0
Diyor <diyorrudov@gmail.com>
2026-01-01 23:53:37 -0700

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    pass
-
+    vsys.stdout.write("$ ")
+    
+# Wait for user input
+    command = input()
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
